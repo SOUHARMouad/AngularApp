e2157b51a4dd6880d890ff4d68fefbaf70fbbd61
mouadOzomaki <souharmouad10gmail.com>
2020-03-01 16:36:13 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { UtilisateursComponent } from './utilisateurs/utilisateurs.component';
+import { PhotosComponent } from './photos/photos.component';
+import {RouterModule,Routes} from '@angular/router';
+import {SrvService} from './srv.service';
+const appRoutes : Routes=[
+  {path: 'Articles',component: ArticlesComponent,},
+  {path: 'Utulisateurs',     component:  UtilisateursComponent,},
+  {path: 'Photos',component:  PhotosComponent,},
+  {path:'',
+  redirectTo: 'about',
+  pathMatch: 'full',
+  }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ArticlesComponent,
+    UtilisateursComponent,
+    PhotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/liste.ts
@@ -0,0 +1,6 @@
+class liste {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+}
